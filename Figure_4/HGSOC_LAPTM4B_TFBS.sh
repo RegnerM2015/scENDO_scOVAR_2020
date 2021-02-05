12263417761b32b08e1f4a931fb27b44d198a2b7
Matt Regner <regnerm@live.unc.edu>
2021-02-05 12:30:19 -0500

    Update HGSOC_LAPTM4B_TFBS.sh

--- Figure_4/HGSOC_LAPTM4B_TFBS.sh
+++ Figure_4/HGSOC_LAPTM4B_TFBS.sh
@@ -23,7 +23,7 @@ tabix -p vcf variants_new.vcf.gz
 #Apply cluster variants to output cluster reference genome
 cat ${dir}/genome.fa | bcftools consensus variants_new.vcf.gz > consensus.fa
 
-# Get sequence of marker enhancers (ranked list of enhancers sorted by FDR)
+# Get sequence of enhancers and promoter
 bedtools getfasta -fi consensus.fa -fo LAPTM4B_enhancer_1.fa -bed enhancer_1.bed
 bedtools getfasta -fi consensus.fa -fo LAPTM4B_enhancer_2.fa -bed enhancer_2.bed
 bedtools getfasta -fi consensus.fa -fo LAPTM4B_enhancer_3.fa -bed enhancer_3.bed
@@ -31,6 +31,7 @@ bedtools getfasta -fi consensus.fa -fo LAPTM4B_enhancer_4.fa -bed enhancer_4.bed
 bedtools getfasta -fi consensus.fa -fo LAPTM4B_enhancer_5.fa -bed enhancer_5.bed
 bedtools getfasta -fi consensus.fa -fo LAPTM4B_promoter.fa -bed promoter.bed
 
+# Run FIMO motif scanning
 fimo --bgfile motif-file --oc enhancer_1_fimo ${dir}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme LAPTM4B_enhancer_1.fa
 fimo --bgfile motif-file --oc enhancer_2_fimo ${dir}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme LAPTM4B_enhancer_2.fa
 fimo --bgfile motif-file --oc enhancer_3_fimo ${dir}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme LAPTM4B_enhancer_3.fa
