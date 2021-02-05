7f4a4c43971f1919056189bf250ab0713921e1bb
Matt Regner <regnerm@live.unc.edu>
2021-02-05 12:27:36 -0500

    Add files via upload

---
+++ Figure_4/HGSOC_LAPTM4B_TFBS.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#SBATCH --job-name Motif 
+#SBATCH --cpus-per-task 8
+#SBATCH -c 8
+#SBATCH --mem 32g
+#SBATCH --partition allnodes
+
+export PATH=/home/regnerm/Software/cellranger-dna-1.1.0:$PATH
+export PATH=/home/regnerm/Software/tabix:$PATH
+export PATH=$HOME/meme/bin:$HOME/meme/libexec/meme-4.12.0:$PATH
+export PATH=/share/apps/install-compute/bin/bcftools:$PATH
+
+dir=/datastore/nextgenout5/share/labs/francolab/scENDO_scOVAR_Proj/scATAC-seq_Processing/HGSOC_ATAC
+
+# # call variants
+#bcftools mpileup -Ou -f ${dir}/genome.fa *.bam | bcftools call -mv -Oz -o variants.vcf.gz
+
+#bcftools index variants.vcf.gz
+#bcftools view --types snps variants.vcf.gz > variants_new.vcf
+#bgzip -c variants_new.vcf > variants_new.vcf.gz
+#tabix -p vcf variants_new.vcf.gz
+
+#Apply cluster variants to output cluster reference genome
+#cat ${dir}/genome.fa | bcftools consensus variants_new.vcf.gz > consensus.fa
+
+# Get sequence of marker enhancers (ranked list of enhancers sorted by FDR)
+bedtools getfasta -fi consensus.fa -fo LAPTM4B_enhancer_1.fa -bed enhancer_1.bed
+bedtools getfasta -fi consensus.fa -fo LAPTM4B_enhancer_2.fa -bed enhancer_2.bed
+bedtools getfasta -fi consensus.fa -fo LAPTM4B_enhancer_3.fa -bed enhancer_3.bed
+bedtools getfasta -fi consensus.fa -fo LAPTM4B_enhancer_4.fa -bed enhancer_4.bed
+bedtools getfasta -fi consensus.fa -fo LAPTM4B_enhancer_5.fa -bed enhancer_5.bed
+bedtools getfasta -fi consensus.fa -fo LAPTM4B_promoter.fa -bed promoter.bed
+#bedtools getfasta -fi consensus.fa -fo LAPTM4B_promoter_11.fa -bed promoter_11.bed
+#bedtools getfasta -fi consensus.fa -fo LAPTM4B_promoter_7_1.fa -bed promoter_7_1.bed
+#bedtools getfasta -fi consensus.fa -fo LAPTM4B_promoter_7_2.fa -bed promoter_7_2.bed
+
+fimo --bgfile motif-file --oc enhancer_1_fimo ${dir}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme LAPTM4B_enhancer_1.fa
+fimo --bgfile motif-file --oc enhancer_2_fimo ${dir}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme LAPTM4B_enhancer_2.fa
+fimo --bgfile motif-file --oc enhancer_3_fimo ${dir}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme LAPTM4B_enhancer_3.fa
+fimo  --bgfile motif-file --oc enhancer_4_fimo ${dir}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme LAPTM4B_enhancer_4.fa
+fimo --bgfile motif-file --oc enhancer_5_fimo ${dir}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme LAPTM4B_enhancer_5.fa
+fimo --bgfile motif-file --oc promoter_fimo ${dir}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme LAPTM4B_promoter.fa
+#fimo --oc promoter_11_fimo ${dir}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme LAPTM4B_promoter_11.fa
+#fimo --oc promoter_7_1_fimo ${dir}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme LAPTM4B_promoter_7_1.fa
+#fimo --oc promoter_7_2_fimo ${dir}/JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme LAPTM4B_promoter_7_2.fa
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Figure_4/TF_expression_FIMO.R
@@ -0,0 +1,46 @@
+
+ovar_HGSOC_scRNA_processed <- readRDS("../ovar_HGSOC_scRNA_processed.rds")
+
+
+hgsoc <-subset(ovar_HGSOC_scRNA_processed,cell.type == "11-Epithelial cell")
+
+counts <- hgsoc@assays$RNA@data
+counts.bulk <- as.data.frame(rowSums(counts))
+
+counts.bulk$gene <- rownames(counts.bulk)
+
+
+
+fimo_outs <- list.files(pattern = "_fimo")
+
+for (i in fimo_outs){
+  fimo <- read.table(paste0(i,"/fimo.txt"))
+  counts.bulk.new <- counts.bulk[counts.bulk$gene %in% fimo$V2,]
+  
+  colnames(fimo)[2] <- "gene"
+  
+  test <- merge(fimo,counts.bulk.new,by = "gene")
+  colnames(test)[11] <- "Expr"
+  
+  test <- dplyr::filter(test,V9 <= 0.1)
+  test <- dplyr::arrange(test,desc(Expr))
+  write.table(test,paste0(i,"_w_expr.txt"),sep = "\t",quote = F,row.names = F,col.names = F)
+}
+
+
+VlnPlot(ovar_HGSOC_scRNA_processed,features = c("KLF6","KLF6","YY1","IRF1"),
+        ncol = 4,pt.size = 0,idents = "11-Epithelial cell",same.y.lims = T)+
+  ggsave("Enhancer_1_vln.pdf",width = 8,height = 4)
+
+VlnPlot(ovar_HGSOC_scRNA_processed,features = c("CEBPD","YY1","YY1","ATF4"),ncol = 4,pt.size = 0,
+        idents = "11-Epithelial cell",same.y.lims = T)+
+  ggsave("Enhancer_2_vln.pdf",width = 8,height = 4)
+  
+  
+  
+  
+  
+  
+  
+library(curatedOvarianData)
+data(package="curatedOvarianData")
