66273cf14019276bc1936484651f1dcb87bbc6c8
Matt Regner <regnerm@live.unc.edu>
2021-06-07 15:25:30 -0400

    Update Figure_2.R

--- Figure_2/Figure_2.R
+++ Figure_2/Figure_2.R
@@ -125,19 +125,15 @@ ov.peaks <- makeGRangesFromDataFrame(ov.peaks)
 atac <- readRDS("final_archr_proj_archrGS-P2Gs.rds")
 # ATAC
 levels(factor(atac$predictedGroup_ArchR))
-my_levels <- as.character(c(11,20,21,22,31,
-               19,34,
-               3,
-               9,10,
-               16,17,
-               0,27,
-               6,8,12,14,15,18,24,25,26,29,
-               7,23,
-               1,33,
-               2,4,30,
-               5,13,
-               32,
-               28,35))
+my_levels <- as.character(c(3,9,10,16,17,
+                            11,20,21,22,31,19,34,
+                            0,27,
+                            6,8,12,14,15,18,24,25,26,29,7,23,
+                            1,33,
+                            2,4,30,
+                            5,13,
+                            32,
+                            28,35))
 
 for ( i in levels(factor(atac$predictedGroup_ArchR))){
   num <-  gsub("-.*","",i)
@@ -260,108 +256,49 @@ plot <- plotBrowserTrack(atac.sub,geneSymbol ="RHEB", groupBy = "predictedGroup_
                          features = GRangesList(TrackA = encode.all,TrackB = ft.peaks,TrackC = ov.peaks), 
                          loops = getPeak2GeneLinks.mod(atac,corCutOff = 0.45,
                                                    PValCutOff = 1e-12,varCutOffATAC = 0,
-                                                   varCutOffRNA = 0),upstream = 7000,downstream = 35000)
+                                                   varCutOffRNA = 0),upstream = 6000,downstream = 35000)
 
 pdf("RHEB_final.pdf",width = 6,height = 8)
 grid::grid.draw(plot[[1]])
 dev.off()
 
-# 
-# # Make violin plots for RHEB expression and mTOR pathway expression
-# # Relevel object@ident
-# rna@active.ident <- factor(x =rna$RNA_snn_res.0.7, levels = rev(my_levels))
-# p1 <- VlnPlot(rna,features = "RHEB",pt.size = 0)+coord_flip()+NoLegend()+ggsave("RHEB_vln_new.pdf",width = 4,height = 8)
-# 
-# p1 <- p1$data
-# colnames(p1)
-# kruskal.test(RHEB~ident,data = p1)
-# 
-# 
-# gset <- read.delim("./BIOCARTA_MTOR_PATHWAY.txt",header = T)
-# gset <- as.character(gset$BIOCARTA_MTOR_PATHWAY[2:length(gset$BIOCARTA_MTOR_PATHWAY)])
-# rna <- AddModuleScore(rna,features = list(gset),name = "mTOR_members",search = T)
-# 
-# 
-# p1 <- VlnPlot(rna,features = "mTOR_members1",pt.size = 0)+coord_flip()+NoLegend()+
-#   ggsave("mTOR_pathway_vln_new.pdf",width = 4,height = 8)
-# 
-# p1 <- p1$data
-# colnames(p1)
-# kruskal.test(mTOR_members1~ident,data = p1)
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# ###########################################################
-# #############################################################
-# # Read in P2G data
-# p2g <- readRDS("./All_P2G_Observed.rds")
-# p2g$RawPVal <- as.numeric(p2g$RawPVal)
-# p2g$Correlation <- as.numeric(p2g$Correlation)
-# 
-# # Plot histogram of Correlations and Pvalues
-# sub <- p2g[p2g$RawPVal <=1e-12,]
-# sub.neg <- sub[sub$Correlation < 0,]
-# lower <- last(sort(sub.neg$Correlation))
-# sub.pos <- sub[sub$Correlation > 0,]
-# upper <- first(sort(sub.pos$Correlation))
-# 
-# min.obs.cor <- min(p2g$Correlation)
-# max.obs.cor <- max(p2g$Correlation)
-# 
-# p1 <- ggplot(sub,aes(x=Correlation))+geom_histogram(color="black",fill="gray90",size=0.3,boundary = 0,bins = 12)+
-#   theme_bw()+geom_vline(xintercept = c(lower,upper),linetype="dashed",size=0.3,color="red")+
-#   scale_y_continuous(expand = c(0,0),limits=c(0,1.5e+06)) +
-#   scale_x_continuous(expand= c(0,0),limits = c(min.obs.cor,max.obs.cor))
-# 
-# min.obs.p <- min(sub$RawPVal)
-# max.obs.p <- max(sub$RawPVal)
-# 
-# p2 <- ggplot(sub,aes(x=RawPVal))+geom_histogram(color="black",fill="gray90",size=0.3,boundary = 0,bins=10)+
-#   theme_bw()+geom_vline(xintercept = 1e-12,linetype="dashed",color="red")+
-#   scale_y_continuous(expand = c(0,0),
-#                      limits = c(0,3.5e+05)) +
-#   scale_x_continuous(expand = c(0,0),
-#                      limits = c(min.obs.p,max.obs.p))
-# 
-# CombinePlots(list(p1,p2),ncol=2)+ggsave("Histrograms.pdf",height =2,width = 6)
-# 
-# # Read in P2G data (null example)
-# # Read in P2G data
-# p2g <- readRDS("./All_P2G_Null_example.rds")
-# p2g$RawPVal <- as.numeric(p2g$RawPVal)
-# p2g$Correlation <- as.numeric(p2g$Correlation)
-# 
-# # Plot histogram of Correlations and Pvalues
-# sub <- p2g[p2g$RawPVal <=1e-12,]
-# sub.neg <- sub[sub$Correlation < 0,]
-# lower <- last(sort(sub.neg$Correlation))
-# sub.pos <- sub[sub$Correlation > 0,]
-# upper <- first(sort(sub.pos$Correlation))
-# 
-# p1 <- ggplot(sub,aes(x=Correlation))+geom_histogram(color="black",fill="gray90",size=0.3,boundary = 0,bins = 12)+
-#   theme_bw()+geom_vline(xintercept = c(lower,upper),linetype="dashed",size=0.3,color="red")+
-#   scale_y_continuous(expand = c(0,0),limits=c(0,100)) +
-#   scale_x_continuous(expand= c(0,0),limits = c(min.obs.cor,max.obs.cor))
-# 
-# p2 <- ggplot(sub,aes(x=RawPVal))+geom_histogram(color="black",fill="gray90",size=0.3,boundary = 0,bins=10)+
-#   theme_bw()+geom_vline(xintercept = 1e-12,linetype="dashed",color="red")+
-#   scale_y_continuous(expand = c(0,0),
-#                      limits = c(0,100)) +
-#   scale_x_continuous(expand = c(0,0),
-#                      limits = c(0,1e-12))
-# 
-# CombinePlots(list(p1,p2),ncol=2)+ggsave("Histrograms-null.pdf",height =2,width = 6)
-# 
+
+
+####################################################################
+# PART 4: plot matching violin plots for RHEB expression and mTOR
+####################################################################
+rna <- readRDS("./endo_ovar_All_scRNA_processed.rds")
+atac.sub$predictedGroup_ArchR <- gsub(".*_","",atac.sub$predictedGroup_ArchR)
+rna.sub <- rna[,rna$cell.type %in% intersect(levels(factor(rna$cell.type)),levels(factor(atac.sub$predictedGroup_ArchR)))]
+
+my_levels <- as.character(c(3,9,10,16,17,
+                            11,20,21,22,31,19,34,
+                            0,27,
+                            6,25,7,
+                            1,33,
+                            2))
+
+# Make violin plots for RHEB expression and mTOR pathway expression
+# Relevel object@ident
+rna.sub@active.ident <- factor(x =rna.sub$RNA_snn_res.0.7, levels = rev(my_levels))
+p1 <- VlnPlot(rna.sub,features = "RHEB",pt.size = 0)+coord_flip()+NoLegend()+ggsave("RHEB_vln_new.pdf",width = 4,height = 8)
+
+p1 <- p1$data
+colnames(p1)
+kruskal.test(RHEB~ident,data = p1)
+
+# Compute mTOR pathway member expression
+gset <- read.delim("./BIOCARTA_MTOR_PATHWAY.txt",header = T)
+gset <- as.character(gset$BIOCARTA_MTOR_PATHWAY[2:length(gset$BIOCARTA_MTOR_PATHWAY)])
+rna.sub <- AddModuleScore(rna.sub,features = list(gset),name = "mTOR_members",search = T)
+
+
+p1 <- VlnPlot(rna.sub,features = "mTOR_members1",pt.size = 0)+coord_flip()+NoLegend()+
+  ggsave("mTOR_pathway_vln_new.pdf",width = 4,height = 8)
+
+p1 <- p1$data
+colnames(p1)
+kruskal.test(mTOR_members1~ident,data = p1)
+ 
 
 
