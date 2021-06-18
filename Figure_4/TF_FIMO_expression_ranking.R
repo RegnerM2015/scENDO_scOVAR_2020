d0172013b2badea8f611038a2304f9300804fda3
Matt Regner <regnerm@live.unc.edu>
2021-06-18 13:46:58 -0400

    Update TF_FIMO_expression_ranking.R

--- Figure_4/TF_FIMO_expression_ranking.R
+++ Figure_4/TF_FIMO_expression_ranking.R
@@ -11,7 +11,7 @@ labels <- c("0-Epithelial cell","7-Epithelial cell","11-Epithelial cell","16-Epi
 hgsoc <- ovar_HGSOC_scRNA_processed[,ovar_HGSOC_scRNA_processed$cell.type %in% labels] 
 
 counts <- hgsoc@assays$RNA@data
-counts.bulk <- as.data.frame(rowSums(counts))
+counts.bulk <- as.data.frame(rowSums(as.matrix(counts)))
 
 counts.bulk$gene <- rownames(counts.bulk)
 
@@ -35,51 +35,49 @@ for (i in fimo_outs){
 
 # Make boxplot for enhancer 2 TF expression levels:
 enh.2.tfs.1 <- data.frame(Data = hgsoc@assays$RNA@data[12582,],
-                          TF ="KLF6")
+                          TF ="A.KLF6")
 print(rownames(hgsoc)[12582])
 enh.2.tfs.2 <- data.frame(Data = hgsoc@assays$RNA@data[15548,],
-                          TF="YY1")
+                          TF="B.YY1")
 print(rownames(hgsoc)[15548])
 enh.2.tfs.3 <- data.frame(Data = hgsoc@assays$RNA@data[6857,],
-                          TF="TFAP2A")
+                          TF="C.TFAP2A")
 print(rownames(hgsoc)[6857])
-enh.2.tfs <- rbind(enh.2.tfs.1,enh.2.tfs.2,enh.2.tfs.3)
-ggplot(enh.2.tfs,aes(x=reorder(TF,-Data, na.rm = TRUE),y=Data))+
-  geom_boxplot()+theme_classic()+ylim(c(0,4))+
-  ggsave("Enhancer_2_vln.pdf",width =5,height = 4)
+
 
 # Make boxplot for enhancer 4 TF expression levels:
 enh.4.tfs.1 <- data.frame(Data = hgsoc@assays$RNA@data[9985,],
-                          TF ="CEBPD")
+                          TF ="D.CEBPD")
 print(rownames(hgsoc)[9985])
 enh.4.tfs.2 <- data.frame(Data = hgsoc@assays$RNA@data[15548,],
-                          TF="YY1")
+                          TF="E.YY1")
 print(rownames(hgsoc)[15548])
 enh.4.tfs.3 <- data.frame(Data = hgsoc@assays$RNA@data[6857,],
-                          TF="TFAP2A")
+                          TF="F.TFAP2A")
 print(rownames(hgsoc)[6857])
-enh.4.tfs <- rbind(enh.4.tfs.1,enh.4.tfs.2,enh.4.tfs.3)
-ggplot(enh.4.tfs,aes(x=reorder(TF,-Data, na.rm = TRUE),y=Data))+
-  geom_boxplot()+theme_classic()+ylim(c(0,4))+
-  ggsave("Enhancer_4_vln.pdf",width =5,height = 4)
 
 # Make boxplot for promoter TF expression levels:
 prom.tfs.1 <- data.frame(Data = hgsoc@assays$RNA@data[12582,],
-                         TF ="KLF6")
+                         TF ="G.KLF6")
 print(rownames(hgsoc)[12582])
 prom.tfs.2 <- data.frame(Data = hgsoc@assays$RNA@data[15260,],
-                         TF="HIF1A")
+                         TF="H.HIF1A")
 print(rownames(hgsoc)[15260])
 prom.tfs.3 <- data.frame(Data = hgsoc@assays$RNA@data[6925,],
-                         TF="SOX4")
+                         TF="I.SOX4")
 print(rownames(hgsoc)[6925])
 prom.tfs.4 <- data.frame(Data = hgsoc@assays$RNA@data[15548,],
-                         TF="YY1")
+                         TF="J.YY1")
 print(rownames(hgsoc)[15548])
-prom.tfs <- rbind(prom.tfs.1,prom.tfs.2,prom.tfs.3,prom.tfs.4)
-ggplot(prom.tfs,aes(x=reorder(TF,-Data, na.rm = TRUE),y=Data))+
-  geom_boxplot()+theme_classic()+ylim(c(0,4))+
-  ggsave("Promoter_vln.pdf",width =5,height = 4)
+
+
+tfs <- rbind(enh.2.tfs.1,enh.2.tfs.2,enh.2.tfs.3,
+                  enh.4.tfs.1,enh.4.tfs.2,enh.4.tfs.3,
+                  prom.tfs.1,prom.tfs.2,prom.tfs.3,prom.tfs.4)
+ggplot(tfs,aes(x=fct_rev(TF),y=Data))+
+  geom_boxplot(lwd=0.45,outlier.size = 0.95,fatten = 0.95)+theme_classic()+ylim(c(0,4))+
+  coord_flip()+
+  ggsave("Vln.pdf",width =4,height = 8)
 
 
 writeLines(capture.output(sessionInfo()), "sessionInfo.txt")
