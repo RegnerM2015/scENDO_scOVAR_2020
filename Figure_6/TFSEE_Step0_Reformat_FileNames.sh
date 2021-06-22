76517180f514e7ea58029e9aa12cd8cf475059d4
Matt Regner <regnerm@live.unc.edu>
2021-06-22 14:28:43 -0400

    Update TFSEE_Step0_Reformat_FileNames.sh

--- Figure_6/TFSEE_Step0_Reformat_FileNames.sh
+++ Figure_6/TFSEE_Step0_Reformat_FileNames.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name Motif 
-#SBATCH --cpus-per-task 8
-#SBATCH -c 8
-#SBATCH --mem 32g
+#SBATCH --job-name Reformat 
+#SBATCH --cpus-per-task 1
+#SBATCH -c 1
+#SBATCH --mem 2g
 #SBATCH --partition allnodes
 
 # Remove whitespace from file names 
