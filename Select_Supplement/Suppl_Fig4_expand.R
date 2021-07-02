c2a5efbce386d790f72a0700da9bc299341ba396
Matt Regner <regnerm@live.unc.edu>
2021-07-01 23:11:23 -0400

    Create Suppl_Fig4_expand.R

---
+++ Select_Supplement/Suppl_Fig4_expand.R
@@ -0,0 +1,89 @@
+library(ggplot2)
+library(Seurat)
+library(scales)
+library(forcats)
+library(stringr)
+library(ArchR)
+
+
+atac <- readRDS("proj_LSI_GeneScores_Annotations_Int.rds")
+atac.df <- plotEmbedding(atac,colorBy = "cellColData",name = "predictedGroup_ArchR",embedding = "UMAP")
+atac.df <- as.data.frame(atac.df$data)
+atac.df$cell.type <- sub(".*?-","",atac.df$color)
+atac.df$cell.type <- as.factor(atac.df$cell.type)
+atac.df$cell.type <- str_replace_all(atac.df$cell.type,"12-Stromal fibroblast","12-Smooth muscle cells")
+atac.df$cluster <-  gsub("-.*","",atac.df$cell.type)
+
+
+my_levels <- c("0","2","3","7","11","15","16","19",
+               "1","4","9","10","22",
+               "14",
+               "5","12","18","21",
+               "6","8","13","20","23",
+               "17")
+
+
+# Relevel object@ident
+atac.df$cluster.new <- factor(x = atac.df$cluster, levels = my_levels)
+
+
+epithelial.cols <- colorRampPalette(c("#A0E989", "#337B24"))
+epithelial.cols <- epithelial.cols(8)
+
+fibro.cols <- c("#FB8CAB","#E65C9C","#CF268A","#Af1281","#6B0772")
+
+endo.cols <- "#377EB8"
+
+t.cols <- c("#CCCCCC","#333333","#666666","#999999")
+
+macro.cols <- colorRampPalette(c("#FECC51", "#FD6A02"))
+macro.cols <- macro.cols(5)
+
+
+b.cols <- "#E41A1C"
+
+
+cols <- c(epithelial.cols,fibro.cols,endo.cols,t.cols,macro.cols,b.cols)
+
+p1 <- ggplot(atac.df,aes(x = x,y=y,color = cluster.new))+
+  geom_point(size = .1)+
+  theme_classic()+
+  theme(plot.title = element_text(face = "bold"))+
+  xlab("UMAP_1")+
+  ylab("UMAP_2")+ 
+  theme(legend.key.size = unit(0.2, "cm"))+
+  scale_color_manual(values = cols)+
+  guides(colour = guide_legend(override.aes = list(size=6)))+NoLegend()
+p1 <- LabelClusters(p1,id="cluster.new",color="black",repel = T,size=8)
+
+# Make pred UMAP
+atac$barcode <- rownames(atac@cellColData)
+atac.df <- plotEmbedding(atac,colorBy = "cellColData",name = "predictedGroup_ArchR",embedding = "UMAP")
+atac.df <- as.data.frame(atac.df$data)
+colnames(atac.df)[3] <- "cluster"
+atac.df$cluster <- sub(".*?-","",atac.df$cluster)
+atac.df$idx <- rownames(atac.df)
+atac$idx <- 1:nrow(atac@cellColData)
+
+atac.df <- dplyr::arrange(atac.df,as.numeric(idx))
+
+length(which(atac$predictedGroup_ArchR==atac.df$cluster))
+atac.df$predictedScore_ArchR <- as.numeric(atac$predictedScore_ArchR)
+
+p2 <- ggplot(atac.df,aes(x = x,y=y,color =predictedScore_ArchR))+
+  geom_point(size = .1)+
+  theme_classic()+
+  theme(plot.title = element_text(face = "bold"))+
+  xlab("UMAP_1")+
+  ylab("UMAP_2")+ 
+  theme(legend.key.size = unit(0.2, "cm"))+
+  scale_color_gradient2(midpoint=0.6,mid="white",low="blue",high="red3")
+p2+ggsave("HGSOC_ATAC_predictionscore.pdf",width = 6,height = 4)
+
+
+ggplot(atac.df,aes(x=predictedScore_ArchR))+
+         geom_histogram(binwidth = 0.025,fill="gray", color="black", alpha=0.9)+
+  theme_classic()+
+  geom_vline(xintercept = 0.50)+ggsave("HGSOC_ATAC_predictionscore_histogram.pdf",width = 4,height = 4)
+
+writeLines(capture.output(sessionInfo()), "sessionInfo.txt")
