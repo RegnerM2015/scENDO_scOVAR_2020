eb5ff26319e91501ed68659661ae6cc9e8bf41e6
Matt Regner <regnerm@live.unc.edu>
2021-07-06 13:09:30 -0400

    Add files via upload

---
+++ Figure_6/TFSEE_Step4_Matrix_Calculations.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name TFSEE
+#SBATCH --cpus-per-task 8
+#SBATCH -c 8
+#SBATCH --mem 64g
+#SBATCH --partition allnodes
+
+#export R_LIBS_USER="/home/regnerm/R/x86_64-pc-linux-gnu-library/4.0:${R_LIBS_USER}"
+source /home/regnerm/anaconda3/etc/profile.d/conda.sh
+conda activate scENDO_scOVAR 
+R CMD BATCH './TFSEE_Step4_Matrix_Calculations.R'
+
+
+
+
+
+
+
